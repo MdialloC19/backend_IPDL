7d6eb57a220466f8a952a3d6ddc3f2f8fe5fd82c
wilfreud <killlergangster@gmail.com>
2024-05-17 16:49:30 +0000

    fix: routes import

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const userRoutes = require("./user.routes");
+const conversationRoutes = require("./conversation.routes");
+const reviewRoutes = require("./review.routes");
 router.use("/user", userRoutes);
 // convos
 router.use("/conversation", conversationRoutes);
